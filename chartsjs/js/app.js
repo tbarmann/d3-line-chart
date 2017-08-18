461153376a33be50010e5d4226961ddbe5c3508f
Timothy Barmann <tbarmann92@gmail.com>
2017-08-18 08:14:13 -0400

    custom tooltip wip

--- chartsjs/js/app.js
+++ chartsjs/js/app.js
@@ -1,7 +1,9 @@
+
 var chartData = {};
 var charts = [];
 var defaultMonthsToShow = 6;
-var chartBackgroundColor = '#212429';
+// var chartBackgroundColor = '#212429';
+var chartBackgroundColor = '#fff';
 var customRed = '#FF4000';
 
 function getData() {
@@ -45,9 +47,8 @@ function buildConfig(history, metal) {
       display:false
     },
     tooltips: {
-      mode: 'index',
-      intersect: false,
-      enabled: false
+      enabled: false,
+      custom: customToolTip(metal)
     },
     legend: {
       display: false
@@ -56,22 +57,47 @@ function buildConfig(history, metal) {
       mode: 'nearest',
       intersect: true
     },
+    elements: {
+      line: {
+        borderWidth: 5
+      },
+      point: {
+        radius: 1,
+        hoverRadius: 10,
+        hitRadius: 10
+      }
+    },
+    annotations:  [{
+      type: 'line',
+      mode: 'vertical',
+      scaleID: 'x-axis',
+      borderColor: '#b6fcd5',
+      borderWidth: 2
+    }],
     scales: {
       xAxes: [{
-        gridLines: {color: '#444444', drawBorder: false},
+        gridLines: {
+          color: '#444444',
+          drawBorder: false,
+          display: false
+        },
         display: true,
         type: 'time',
         unit: 'month',
         unitStepSize: 1,
         time: {
-          displayFormats: {'month': 'MMM YYYY'}
+          displayFormats: {'month': 'MMM YYYY'},
+          unit: 'month'
         },
         scaleLabel: {
           display: false
         }
       }],
       yAxes: [{
-        gridLines: {display: false},
+        gridLines: {
+          display: false,
+          drawBorder: false
+        },
         display: true,
         scaleLabel: {
           display: false
@@ -88,12 +114,13 @@ function buildConfig(history, metal) {
       datasets: [{
         label: 'Actual',
         lineTension: 0,
+        lineBorderWidth: 5,
         backgroundColor: customRed,
         borderColor:  customRed,
         data: prices,
         fill: false,
-        pointHoverRadius: 4,
-        pointHighlihtStroke: 4,
+        pointHoverRadius: 7,
+        pointHoverBorderWidth: 5,
         pointHoverBackgroundColor: chartBackgroundColor
       }]
     },
@@ -115,4 +142,97 @@ $('.period-selector input[type=radio]').on('change', function() {
   charts[metal].config = newConfig;
   charts[metal].update();
 });
+
+
+function customToolTip(metal) {
+  return function(tooltipModel) {
+
+    // Tooltip Element
+    var tooltipEl = document.getElementById('chartjs-tooltip-' + metal);
+
+    // Create element on first render
+    if (!tooltipEl) {
+      tooltipEl = document.createElement('div');
+      tooltipEl.id = 'chartjs-tooltip-' + metal;
+      tooltipEl.innerHTML = "<table></table>"
+      document.body.appendChild(tooltipEl);
+    }
+
+    // Hide if no tooltip
+    if (tooltipModel.opacity === 0) {
+      // tooltipEl.style.opacity = 0;
+    return;
+    }
+
+    // Set caret Position
+    tooltipEl.classList.remove('above', 'below', 'no-transform');
+    if (tooltipModel.yAlign) {
+      tooltipEl.classList.add(tooltipModel.yAlign);
+    } else {
+      tooltipEl.classList.add('no-transform');
+    }
+
+    function getBody(bodyItem) {
+      return bodyItem.lines;
+    }
+
+    // Set Text
+    if (tooltipModel.body) {
+      var titleLines = tooltipModel.title || [];
+      var bodyLines = tooltipModel.body.map(getBody);
+
+      var innerHtml = '<thead>';
+
+      titleLines.forEach(function(title) {
+        innerHtml += '<tr><th>' + title + '</th></tr>';
+      });
+      innerHtml += '</thead><tbody>';
+
+      bodyLines.forEach(function(body, i) {
+        var colors = tooltipModel.labelColors[i];
+        var style = 'background:' + colors.backgroundColor;
+        style += '; border-color:' + colors.borderColor;
+        style += '; border-width: 2px';
+        var span = '<span class="chartjs-tooltip-key" style="' + style + '"></span>';
+        innerHtml += '<tr><td>' + span + body + '</td></tr>';
+      });
+      innerHtml += '</tbody>';
+
+      var tableRoot = tooltipEl.querySelector('table');
+      tableRoot.innerHTML = innerHtml;
+    }
+
+    // `this` will be the overall tooltip
+    var position = this._chart.canvas.getBoundingClientRect();
+
+    // Display, position, and set styles for font
+    tooltipEl.style.opacity = 1;
+    tooltipEl.style.left = position.left + tooltipModel.caretX + 'px';
+    tooltipEl.style.top = position.top + tooltipModel.caretY + 'px';
+    tooltipEl.style.fontFamily = tooltipModel._fontFamily;
+    tooltipEl.style.fontSize = tooltipModel.fontSize;
+    tooltipEl.style.fontStyle = tooltipModel._fontStyle;
+    tooltipEl.style.padding = tooltipModel.yPadding + 'px ' + tooltipModel.xPadding + 'px';
+  }
+}
+
+
+// https://stackoverflow.com/questions/43113796/chart-js-vertical-crosshair-vertical-annotation-that-moves-with-mouse-in-lin
+// $(document).ready(function(){
+//   canvas.onmousemove = function (evt) {
+//         var points = myChart.getElementsAtXAxis(evt);
+//         annotation.annotations[0].value = new Date(myChart.config.data.labels[points[0]._index]);
+//         myChart.update();
+//     };
+/// / });
+
+
+// crosshairs
+// https://jsfiddle.net/8fp3uutt/
+
+// gradient line:
+// https://blog.vanila.io/chart-js-tutorial-how-to-make-gradient-line-chart-af145e5c92f9
+
+// tooltip docs
+// https://github.com/chartjs/Chart.js/blob/3e94b9431a5b3d6e4bfbfd6a2339a350999b3292/docs/configuration/tooltip.md#custom-tooltips
 
